e922edf042efdac6af0768bbccb3d3a0c251ef80
FaridAshidiq <l200190155@student.ums.ac.id>
2022-06-22 14:36:37 +0700

    Tugas Psee

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style1.css">
+<style>
+a:link, a:visited {
+  background-color: blue;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: pink;
+}
+</style>
+</head>
+<body style="background: url(img/bg1.jpg) " class="trans">
+<div class="wrapper">
+<center><p><b><font size="17">METODE DEPRESIASI</font></b></p></center></br></br></br>
+            <center><a href="straight_line.php"><font size="5">Straight Line</font></a></br></br>
+            <a href="reducing_balance.php"><font size="5">Reducing Balance</font></a></br></br>
+            <a href="sum_of_the_year.php"><font size="5">Sum of The Year</font></a></br></br>
+            <a href="unit_of_activity.php"><font size="5">Unit of Activity</font></a></br></br></center>
+</div>
+<div id="footer">
+        <p><center> Copyright &copy; <?php echo date("Y")?> - L200190122| L200190155</p></center>
+    </div>
+</body>
+</html>
